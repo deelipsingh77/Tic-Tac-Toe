77ff394b76c5b2402f533df8697b8a5ec7467de9
Deelip Kumar Singh <deelipsingh77@gmail.com>
2023-10-01 16:05:08 +0530

    Added Depth Logic

--- cpu_logic.py
+++ cpu_logic.py
@@ -23,32 +23,40 @@ def cpu_move(player):
         if score > best_score:
             best_score = score
             best_move = move
+        elif score == best_score:
+            current_depth = get_depth(board.main_board, move, player)
+            best_depth = get_depth(board.main_board, best_move, player)
+            if current_depth < best_depth:
+                best_move = move
     insert_piece(board.main_board, best_move, player['cpu'])
 
 def minimax(board, depth, is_maximizer, player):
     win, winner = check_win(board)
     is_draw = check_draw(board)
-    if win or is_draw:
-        if win:
-            score = scores.get(winner)
-            return score
-        elif is_draw:
-            score = scores.get('tie')
-            return score
+    if win:
+        return scores[winner]
+    elif is_draw:
+        return scores['tie']
+    
+    if is_maximizer:
+        best_score = -math.inf
+        for move in possible_moves(board):
+            insert_piece(board, move, player['human'])
+            score = minimax(board, depth+1, False, player)
+            empty_cell(board, move)
+            best_score = max(score, best_score)
+        return best_score
     else:
-        if is_maximizer:
-            best_score = -math.inf
-            for move in possible_moves(board):
-                insert_piece(board, move, player['human'])
-                score = minimax(board, depth+1, False, player)
-                empty_cell(board, move)
-                best_score = max(score, best_score)
-            return best_score
-        else:
-            best_score = math.inf
-            for move in possible_moves(board):
-                insert_piece(board, move, player['cpu'])
-                score = minimax(board, depth+1, True, player)
-                empty_cell(board, move)
-                best_score = min(score, best_score)
-            return best_score
+        best_score = math.inf
+        for move in possible_moves(board):
+            insert_piece(board, move, player['cpu'])
+            score = minimax(board, depth+1, True, player)
+            empty_cell(board, move)
+            best_score = min(score, best_score)
+        return best_score
+    
+def get_depth(board, move, player):
+    insert_piece(board, move, player['cpu'])
+    depth = minimax(board, 0, True, player)
+    empty_cell(board, move)
+    return depth
